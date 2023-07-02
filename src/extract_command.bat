260addf630ce6cf7abea637f2a558bf6b8f4f9e2
刘杰文 <1635370493@qq.com>
2023-07-02 22:17:48 +0800

    bad attempt of exe version adjustments.

--- src/extract_command.bat
+++ src/extract_command.bat
@@ -1,9 +1,18 @@
 mkdir res_ico
+@set para_0th=%0
 @echo Pass the path of the folder that is going to be extracted from.
 @echo You may want a clean res_ico directory. Do this yourself.
 @if "%1" == "" @(
     echo No input. Exit.
+    call:exit_p1
     goto:eof
 )
 
 for /f "delims=" %%i in ('dir "%1" /A:A /b') do @( echo %%i & xcopy /H /R /K "%1\%%i" res_ico\ ) 
+
+:exit_p1
+@if /I "%para_0th:~-4,-1%" == ".exe" @(
+    pause
+)
+@goto:eof
+
